5c8b37e4649d6ab51d90f29028218f3abf771d37
Ashwin Prasad <ashwin@onpar.in>
2021-04-10 17:28:40 +0530

    latest working version

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var privateKey = fs.readFileSync('localhost-key.pem');
 var certificate = fs.readFileSync('localhost.pem');
 
 const app = express()
-
+express.static.mime.define({ 'text/plain': ['gltf'] });
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile)
 app.use('/obj', express.static('3d-models'))
 app.use('/markers', express.static('markers'))
 app.get('/', (req, res) => res.render('index.html'))
+app.get('/default', (req, res) => res.render('default.html'))
+app.get('/default2', (req, res) => res.render('default2.html'))
 
 const port = 5678
 https.createServer({
